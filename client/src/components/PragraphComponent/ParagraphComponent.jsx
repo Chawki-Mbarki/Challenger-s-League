9dae9157664290d940aaf0399a15716a04a7c00b
Saber-GHR <gharbi.saber.tn@gmail.com>
2024-10-01 20:17:32 +0100

    client: cretaed paragraph component
    gsdgddhqdjkhdjk

---
+++ client/src/components/PragraphComponent/ParagraphComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './paragragraph.module.css'
+function ParagraphComponent() {
+  return (
+    <p className= {styles.redparagraph}>ParagraphComponenttetsttttttttttttttttt</p>
+  )
+}
+
+export default ParagraphComponent
