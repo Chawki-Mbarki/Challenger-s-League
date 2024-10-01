9fc76526c5da2c691d406d68ac7542d1d0049fa9
ChawkiMbarki <77485409+Chawki-Mbarki@users.noreply.github.com>
2024-10-01 07:09:27 -0700

    Project Initialization

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { ProtectedRoute } from './components'
+
+import './app.css'
+
+
+function App() {
+  return (
+    <Routes>
+      {/* 
+        //! put routes here
+        //? , Normal Route example: <Route path="/login" component={Login} />
+        //? , Protected Route example: <ProtectedRoute path="/dashboard" component={Dashboard} />
+      */}
+    </Routes>
+  )
+}
+
+export default App
