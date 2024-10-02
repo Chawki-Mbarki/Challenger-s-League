6da3aebaa239ecb1504d999c167bd63b8d33038a
ChawkiMbarki <77485409+Chawki-Mbarki@users.noreply.github.com>
2024-10-02 07:42:16 -0700

    Exported Btn, Input, Paragraph Component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,1 +1,4 @@
 export { default as ProtectedRoute } from './ProtectedRoute/ProtectedRoute'
+export { default as Btn } from './Btn/Btn'
+export { default as Paragraph } from './Paragraph/Paragraph'
+export { default as Input } from './Input/Input'
