4acd49e58559fe31f660199d12f0058da7441dd9
ChawkiMbarki <77485409+Chawki-Mbarki@users.noreply.github.com>
2024-10-02 07:41:44 -0700

    Recreated The Button Component

--- client/src/components/Btn/Btn.jsx
+++ client/src/components/Btn/Btn.jsx
@@ -1,39 +1,12 @@
-import React from 'react'
+import React from "react";
+import styles from "./Btn.module.css";
 
+const Btn = ({ type, style, text, onClick }) => {
+  return (
+    <button type={type} className={styles[style]} onClick={onClick}>
+      {text}
+    </button>
+  );
+};
 
-const Btn = () => {
-return (
-<div>
-
-    <div >
-    <label for="username"></label>
-    <input  className='formOne'  type="text" id="username" name="username"></input>
-    </div>
-
-    <div>
-    <button className="red-button">Primary</button>
-    <button className="white-button">Secondary</button>
-    <button className="black-button">White</button>
-    </div>
-
-    <div>
-    <button className="yes-button">Yes</button>
-    <button className="no-button">No</button>
-    <button className="pick-button">Pick</button>
-    </div>
-
-    <div>
-    <p className='paragraph'>
-        challenger league lets you challaenge players in random or draft pick games,
-        connect xith nex friends , and track your match history , win rate , and champion perfermance
-
-    </p>
-    </div>
-    
-
-</div>
-
-)
-}
-
-export default Btn
+export default Btn;
