8204a59aa500fa3a207b7c9f41b088447481d909
ChawkiMbarki <hajji2002ahmed@gmail.com>
2024-10-03 00:19:13 +0100

    Server: adjusted user model and routes

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  passwordHash: { type: String, required: true },
+  password: { type: String, required: true },
   XP: { type: Number, default: 0 },
   level: { type: Number, default: 0 },
   friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
