bb17460bbf4374453c158826d51c9136166d6a68
ChawkiMbarki <77485409+Chawki-Mbarki@users.noreply.github.com>
2024-10-02 11:27:09 -0700

    Server: Fix Authentication Issue

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken')
 
-module.exports.verifyToken = (req, res, next) => {
-  const token = req.header('Authorization')
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1]
   if (!token) return res.status(401).json({ message: 'Access denied' })
   
   try {
