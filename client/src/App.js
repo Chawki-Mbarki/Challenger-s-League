ba0f68b80307efb512eb0da92ab09f43e3296b5d
ChawkiMbarki <hajji2002ahmed@gmail.com>
2024-10-03 00:26:32 +0100

    Client : Added a route for the Home View

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { ProtectedRoute } from './components'
-import { TestArea } from './views'
+import { TestArea, Home } from './views'
 
 import './app.css'
 
@@ -10,6 +10,7 @@ function App() {
   return (
     <Routes>
       <Route path="/TestArea" element={<TestArea />} />
+      <Route path="/" element={<Home />} />
       {/* 
         //! put routes here
         //? , Normal Route example: <Route path="/login" element={Login} />
