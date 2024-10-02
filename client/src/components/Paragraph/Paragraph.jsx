cd221a110276a35ccd72d60435834fa1b6af6c92
ChawkiMbarki <77485409+Chawki-Mbarki@users.noreply.github.com>
2024-10-02 07:39:32 -0700

    Created Paragraph Component

---
+++ client/src/components/Paragraph/Paragraph.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Styles from "./Paragraph.module.css";
+
+const Paragraph = ({text}) => {
+  return (
+    <div className={Styles.paragraphContainer}>
+      <p className={Styles.paragraph}>{text}</p>
+    </div>
+  );
+}
+
+export default Paragraph;
