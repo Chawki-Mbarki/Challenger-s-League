b905acd9128a40c2b49d5e080d72cc6692039306
ChawkiMbarki <77485409+Chawki-Mbarki@users.noreply.github.com>
2024-10-16 06:28:55 -0700

    Client: Created different widget contents

---
+++ client/src/containers/ChallengeWidget/WidgetContent/index.js
@@ -0,0 +1,5 @@
+export { default as ChallengeStart } from './ChallengeStart';
+export { default as WaitingForResponse } from './WaitingForResponse';
+export { default as DraftPickChampionSelection } from './DraftPickChampionSelection';
+export { default as WaitingForOtherPlayerToPick } from './WaitingForOtherPlayerToPick';
+export { default as Result } from './Result';
