688c16bc6317fd97712e058bc3e6e0d0e5aa8426
ChawkiMbarki <77485409+Chawki-Mbarki@users.noreply.github.com>
2024-10-16 06:12:35 -0700

    Server: Updated Existing Models

--- server/models/challenge.model.js
+++ server/models/challenge.model.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose');
+const championSchema = require('./champion.model');
 
 const challengeSchema = new mongoose.Schema({
   challenger: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  challengerChamp: { type: championSchema, default: null },
+  challengerResult: { type: Boolean, default: false },
   opponent: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  opponentChamp: { type: championSchema, default: null },
+  opponentResult: { type: Boolean, default: false },
   status: { type: String, enum: ['pending', 'accepted', 'declined'], default: 'pending' },
+  state: { type: String, enum: ['waiting', 'Blind Pick', 'Draft Pick', 'result'], default: 'waiting' },
   gameMode: { type: String, enum: ['Blind Pick', 'Draft Pick'], required: true },
   createdAt: { type: Date, default: Date.now },
   expiresAt: { type: Date, required: true }
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model('Challenge', challengeSchema);
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -4,10 +4,8 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  XP: { type: Number, default: 0 },
-  level: { type: Number, default: 0 },
   friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   blockedUsers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
